261f6dfb2f1b8cbbe3de07a452b05281f7dedcbb
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-05-23 20:38:51 +0900

    feat: boardList 출력 및 view 기능 구현

---
+++ src/main/java/org/example/boardweb/controller/BoardController.java
@@ -0,0 +1,21 @@
+package org.example.boardweb.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.boardweb.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/boards")
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public String showBoardList(Model model) {
+        model.addAttribute("boardList",boardService.findAll());
+        return "/boards/list";
+    }
+}
---
+++ src/main/java/org/example/boardweb/service/BoardService.java
@@ -0,0 +1,18 @@
+package org.example.boardweb.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.boardweb.domain.Board;
+import org.example.boardweb.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    @Transactional(readOnly = true)
+    public Iterable<Board> findAll() {
+        return boardRepository.findAll();
+    }
+}
